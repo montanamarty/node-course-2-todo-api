a3e926b1ddd6cc55d1a6f4a8d4ae9e5b009b7787
MontanaMarty <mtmarty@cyberport.net>
2018-03-23 17:37:25 -0600

    add post todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./models/user');
+var {Todo} = require('./models/todo');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text,
+    completed: req.body.completed
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+})
+
+// var otherTodo = new Todo({
+//   text: '    Edit this video      ',
+//   // completed: true,
+//   // completedAt: 1521840532
+// })
+//
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save', e);1
+// });
+
+// var newUser = new User({
+//   email: '   Adam@workaholic.me     '
+// });
+//
+// newUser.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save', e);
+// });
