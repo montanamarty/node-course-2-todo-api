50a48634c5a7b991b283c6d75c158ba839f3c207
MontanaMarty <mtmarty@cyberport.net>
2018-03-24 16:14:53 -0600

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text,
-    completed: req.body.completed
+    // completed: req.body.completed
   });
 
   todo.save().then((doc) => {
@@ -25,6 +25,8 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 })
+
+module.exports = {app};
 
 // var otherTodo = new Todo({
 //   text: '    Edit this video      ',
