ed91eafbf94f760a032f9536a6535fcc92c84cf8
MontanaMarty <mtmarty@cyberport.net>
2018-04-22 15:45:20 -0600

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -113,6 +113,7 @@ app.patch('/todos/:id', (req, res) => {
 // });
 // var body = _.pick(req.body, ['text', 'completed']);
 
+// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password'])
   // var user = new User({
@@ -172,9 +173,22 @@ app.get('/users/me', authenticate, (req, res) => {
 //   });
 // });
 
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
-})
+});
 
 module.exports = {app};
 
