ef9b984f06945a51664d218d3bf4f02dd38f0a65
MontanaMarty <mtmarty@cyberport.net>
2018-05-20 18:24:12 -0600

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,23 @@
 var env = process.env.NODE_ENV || 'developement';
 console.log('env ***', env);
 
-if (env === 'developement') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'developement' || env === 'test') {
+  let config = require('./config.json');
+  // console.log(config);
+  // uncomment the above and run node ./server/config/config.js to see config
+  let envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    // console.log(`${key}  = ${envConfig[key]}`);
+    // uncomment and run node ./server/config/config.js
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if (env === 'developement') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// }else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
